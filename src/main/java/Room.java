7129bddfec8c7b13f8bc93043ad5ec15e1d92071
Farnoosh Johnson, Fernanda Lowe, Reed Lambier and Scott McIntire <farnoosh+fernanda+reed+scott@>
2016-05-16 11:52:02 -0700

    pass save test

--- src/main/java/Room.java
+++ src/main/java/Room.java
@@ -5,15 +5,56 @@ import java.util.ArrayList;
 
 
 public class Room {
-  // private int id;
-  // private String type;
-  // private String address;
-  // private String url;
+  private int id;
+  private String type;
+  private String address;
+  private String url;
 
   public Room (String type, String address, String url){
-    // this.type = type;
-    // this.address = address;
-    // this.url = url;
+    this.type = type;
+    this.address = address;
+    this.url = url;
+  }
+  public String getType(){
+    return type;
+  }
+  public String getAddress(){
+    return address;
+  }
+  public String getUrl(){
+    return url;
+  }
+  public int getId(){
+    return id;
+  }
+  public static List<Room> all(){
+    String roomsTable = "SELECT id, type, address, url FROM rooms";
+      try (Connection con = DB.sql2o.open()){
+        return con.createQuery(roomsTable).executeAndFetch(Room.class);
+    }
+  }
+
+  @Override
+  public boolean equals(Object otherRoomInfo) {
+    if(!(otherRoomInfo instanceof Room)){
+      return false;
+    } else {
+      Room newRoom = (Room) otherRoomInfo;
+      return this.getId() == newRoom.getId();
+    }
+  }
+
+  public void save() {
+    try (Connection con = DB.sql2o.open()){
+      String roomsTable = "INSERT INTO rooms (type, address, url) VALUES (:type, :address, :url)";
+      this.id = (int) con.createQuery(roomsTable, true)
+        .addParameter("type", this.type)
+        .addParameter("address", this.address)
+        .addParameter("url", this.url)
+        .executeUpdate()
+        .getKey();
+      }
+
   }
 
 
--- src/test/java/RoomTest.java
+++ src/test/java/RoomTest.java
@@ -10,9 +10,49 @@ public class RoomTest {
   public DatabaseRule database = new DatabaseRule();
 
   @Test
- public void Room_instantiatesCorrectly_true() {
-   Room newRoom= new Room("room", "400 SW 6th Ave, Portland, OR, 97202", "http://lorempixel.com/400/200/");
-   assertEquals(true, newRoom instanceof Room);
- }
+  public void Room_instantiatesCorrectly_true() {
+    Room newRoom= new Room("room", "400 SW 6th Ave, Portland, OR, 97202", "http://lorempixel.com/400/200/");
+    assertEquals(true, newRoom instanceof Room);
+  }
+  @Test
+  public void getRoom_type_address_url_String() {
+    Room newRoom = new Room("room", "400 SW 6th Ave, Portland, OR, 97202", "http://lorempixel.com/400/200/");
+    assertEquals("room", newRoom.getType());
+    assertEquals("400 SW 6th Ave, Portland, OR, 97202", newRoom.getAddress());
+    assertEquals("http://lorempixel.com/400/200/", newRoom.getUrl());
+  }
+  @Test
+  public void all_emptyAtFirst_0() {
+    assertEquals(0, Room.all().size());
+  }
+  @Test
+  public void equals_returnsTrueIf_type_address_url_AretheSame_true() {
+    Room inputRoom = new Room("room", "400 SW 6th Ave, Portland, OR, 97202", "http://lorempixel.com/400/200/");
+    Room databaseRoom = new Room("room", "400 SW 6th Ave, Portland, OR, 97202", "http://lorempixel.com/400/200/");
+    assertTrue(inputRoom.equals(databaseRoom));
+  }
+
+   @Test
+   public void save_saveRoomIntoDataBase_true() {
+     String roomType ="type";
+     String roomAddress ="address";
+     String roomUrl ="url";
+     Room expectedRoom = new Room(roomType, roomAddress, roomUrl);
+     expectedRoom.save();
+     assertTrue(Room.all().get(0).equals(expectedRoom));
+   }
+   @Test
+   public void save_assignsIdToRoom_int() {
+     String roomType ="type";
+     String roomAddress ="address";
+     String roomUrl ="url";
+     Room expectedRoom = new Room(roomType, roomAddress, roomUrl);
+     Room savedRoom = Room.all().get(0);
+     assertEquals(expectedRoom.getId(), savedRoom.getId());
+   }
+
+
+
+
 
 }
