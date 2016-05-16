0cc64a252674847cfcfc2e50e621e33fadbd4595
Reed Lambier <rlambier46@gmail.com>
2016-05-16 11:18:07 -0700

    fixed testing and review logic

--- src/main/java/Review.java
+++ src/main/java/Review.java
@@ -1,7 +1,6 @@
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.ArrayList;
 import org.sql2o.*;
+import java.util.*;
 
 public class Review {
   private String user_name;
@@ -16,7 +15,7 @@ public class Review {
     this.description = description;
     this.rating = rating;
   }
-  
+
   public int getId() {
     return id;
   }
@@ -46,8 +45,9 @@ public class Review {
       Review newReview = (Review) obj;
       return this.getId() == newReview.getId() && this.getRating() == newReview.getRating() &&
       this.getUserName().equals(newReview.getUserName()) &&
-      this.getDescription().equals(newReview.getDescription()) &&
-      this.getDate().equals(newReview.getDate());
+      this.getDescription().equals(newReview.getDescription());
+      // &&
+      // this.getDate().equals(newReview.getDate());
     }
   }
 
@@ -112,7 +112,7 @@ public class Review {
       }
     }
 
-    if(newRating != null) {
+    if(newRating != 0) {
       try(Connection con = DB.sql2o.open()) {
         String sql = "UPDATE reviews SET rating = :newRating WHERE id = :id;";
         con.createQuery(sql)
--- src/test/java/ReviewTest.java
+++ src/test/java/ReviewTest.java
@@ -4,9 +4,48 @@ import java.time.LocalDateTime;
 import org.sql2o.*;
 import java.util.List;
 
-public class VenueTest {
+public class ReviewTest {
 
   @Rule
   public DatabaseRule database = new DatabaseRule();
 
+//WILL INSTANTIATE
+  @Test
+  public void review_reviewInstantiatesCorrectly_True() {
+    Review testReview = new Review("Bob Dillon", "The room was aight.", 4);
+    assertEquals(true, testReview instanceof Review);
+  }
+
+//WILL INSTANTIATE WITH DESCRIPTION
+  @Test
+  public void getDescription_reviewInstantiatesWithDescription_String() {
+    Review testReview = new Review("Mark Antony", "magnifico", 5);
+    assertEquals("magnifico", testReview.getDescription());
+  }
+
+//WILL INSTANTIATE WITH RATING
+  @Test
+  public void getRating_reviewInstantiatesWithRating() {
+    Review testReview = new Review("Mark Twain", "T'was awful, indeed", 2);
+    assertEquals(2, testReview.getRating());
+  }
+
+  // WILL CHECK TO MAKE SURE DATE IS WORKING
+  // @Test
+  // public void getDate_reviewWithDate() {
+  //   Review testReview = new Review("")
+  // }
+
+  //WILL EMPTY STORED FROM TEST DATABASE AFTER EACH TEST
+  @Test
+  public void all_emptyAtFirst() {
+    assertEquals(Review.all().size(), 0);
+  }
+
+// //WILL
+//   @Test
+//   public void getId_assignsIdToObject() {
+//     Review myReview = new Review("Shakira", "Muy Bien" 4);
+//     myReview.save
+//   }
 }
