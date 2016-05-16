596f54dfc8f4b084ff6be3fa885695287f3e3616
Fernanda Lowe <fe_lowe@hotmail.com>
2016-05-16 10:14:59 -0700

    add CRUD methods for Review object

--- src/main/java/Review.java
+++ src/main/java/Review.java
@@ -51,17 +51,75 @@ public class Review {
     }
   }
 
-  // public void save() {
-  //   try(Connection con = DB.sql2o.open()) {
-  //     String sql = "INSERT INTO reviews();";
-  //     this.id = (int) con.createQuery(sql, true)
-  //       .addParameter("rating", this.rating)
-  //       .addParameter("name", this.name)
-  //       .addParameter("instructions", this.instructions)
-  //       .addParameter("ingredients", this.ingredients)
-  //       .addParameter("date_created", this.getDate())
-  //       .executeUpdate()
-  //       .getKey();
-  //   }
-  // }
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO reviews(description, user_name, date_created, rating) VALUES (:description, :user_name, :date_created, :rating)";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("rating", this.rating)
+        .addParameter("user_name", this.user_name)
+        .addParameter("description", this.description)
+        .addParameter("date_created", this.getDate())
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  public static List<Review> all() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM reviews";
+      List<Review> all = con.createQuery(sql)
+        .executeAndFetch(Review.class);
+      return all;
+    }
+  }
+
+  public static Review find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM reviews WHERE id=:id;";
+      return con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Review.class);
+    }
+  }
+
+  public void delete() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "DELETE FROM reviews WHERE id=:id;";
+      con.createQuery(sql)
+        .addParameter("id", this.getId())
+        .executeUpdate();
+    }
+  }
+
+  public void update(String newUser_name, String newDescription, int newRating) {
+    if(newUser_name.trim().length() != 0) {
+      try(Connection con = DB.sql2o.open()) {
+        String sql = "UPDATE reviews SET user_name = :newUser_name WHERE id = :id;";
+        con.createQuery(sql)
+          .addParameter("user_name", newUser_name)
+          .addParameter("id", id)
+          .executeUpdate();
+      }
+    }
+
+    if(newDescription.trim().length() != 0) {
+      try(Connection con = DB.sql2o.open()) {
+        String sql = "UPDATE reviews SET description = :newDescription WHERE id = :id;";
+        con.createQuery(sql)
+          .addParameter("description", newDescription)
+          .addParameter("id", id)
+          .executeUpdate();
+      }
+    }
+
+    if(newRating != null) {
+      try(Connection con = DB.sql2o.open()) {
+        String sql = "UPDATE reviews SET rating = :newRating WHERE id = :id;";
+        con.createQuery(sql)
+          .addParameter("rating", newRating)
+          .addParameter("id", id)
+          .executeUpdate();
+      }
+    }
+  }
 }
