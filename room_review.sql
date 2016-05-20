6bc524f322ffc352e4b9935123eee2b59cb58d10
Fernanda Lowe <fernanda@>
2016-05-20 09:54:22 -0700

    add database file

--- room_review.sql
+++ room_review.sql
@@ -161,6 +161,18 @@ ALTER TABLE ONLY users ALTER COLUMN id SET DEFAULT nextval('users_id_seq'::regcl
 --
 
 COPY reviews (id, description, date_created, room_id, user_id, rating) FROM stdin;
+4	"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum do	2016-05-19 15:14:02.786	15	31	4.png
+5	Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laboru	2016-05-19 15:15:36.648	16	32	2.png
+6	Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laboru	2016-05-19 15:16:38.487	17	33	3.png
+7	Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 	2016-05-19 15:18:05.967	18	34	3.png
+8	Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo volu	2016-05-19 15:19:33.121	19	35	2.png
+9	"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipi	2016-05-19 15:20:52.399	20	36	\N
+10	At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit	2016-05-19 15:22:09.91	21	37	5.png
+11	Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo	2016-05-19 15:48:05.375	22	38	1.png
+12	Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro qu	2016-05-19 15:48:56.415	23	39	5.png
+13	At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiore	2016-05-19 15:50:04.736	24	40	2.png
+14	At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumqueuibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiore	2016-05-19 15:51:37.847	25	41	2.png
+15	Test	2016-05-20 09:15:53.153	26	45	1.png
 \.
 
 
@@ -168,7 +180,7 @@ COPY reviews (id, description, date_created, room_id, user_id, rating) FROM stdi
 -- Name: reviews_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Guest
 --
 
-SELECT pg_catalog.setval('reviews_id_seq', 2, true);
+SELECT pg_catalog.setval('reviews_id_seq', 15, true);
 
 
 --
@@ -176,6 +188,18 @@ SELECT pg_catalog.setval('reviews_id_seq', 2, true);
 --
 
 COPY rooms (id, type, address, url) FROM stdin;
+15	apartment	2101 NE 8th ave at Tillamook, Portland	https://portland.craigslist.org/mlt/roo/5594685912.html
+16	loft	SE 41st at SE Morrison	https://portland.craigslist.org/mlt/roo/5594642856.html
+17	apartment	SW 5th Ave at College Street, Portland	
+18	loft	434 SW College St	http://all4desktop.com/data_images/original/4240860-bedroom.jpg
+19	apartment	Brazee ct at 138 th pl, Portland	http://globe-views.com/dreams/bedroom.html
+20	loft	164 th avenue, Porland	https://upload.wikimedia.org/wikipedia/commons/c/cf/Hotel-suite-bedroom.jpg
+21	apartment	Stark and Sundial at Sundial, Porland	http://tremendouswallpapers.com/wp-content/uploads/2014/12/deluxe-bedroom-1920x1080.jpg
+22	apartment	NE 154th at Ward Road	http://photos2.zillowstatic.com/i_f/ISlio5u5uktg270000000000.jpg
+23	apartment	107 N Cook St, Portland	http://livinator.com/wp-content/uploads/2015/11/hgtv21.jpg
+24	apartment	SE foster rd at Harold St, Portland	http://www.simplycleanhome.com/wp-content/uploads/2014/06/blog-post-10-image.jpg
+25	apartment	8604 SW Scoffins, Portland	http://cdn.home-designing.com/wp-content/uploads/2014/09/artist-bedroom-attic.jpeg
+26	apartment	2101 NE 8th ave at Tillamook, Portland	
 \.
 
 
@@ -183,7 +207,7 @@ COPY rooms (id, type, address, url) FROM stdin;
 -- Name: rooms_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Guest
 --
 
-SELECT pg_catalog.setval('rooms_id_seq', 13, true);
+SELECT pg_catalog.setval('rooms_id_seq', 26, true);
 
 
 --
@@ -191,6 +215,22 @@ SELECT pg_catalog.setval('rooms_id_seq', 13, true);
 --
 
 COPY users (id, name, password) FROM stdin;
+30	Fernanda	234567
+31	Fernanda	234567
+32	Fernanda	Lowe
+33	Fernanda	Lowe
+34	Fernanda	234567
+35	Fe_lowe@hotmail.com	Lowe
+36	Fernanda	234567
+37	Fernanda	234567
+38	Fernanda	Lowe
+39	Fernanda	234567
+40	Fernanda	Lowe
+41	Fe_lowe@hotmail.com	Lowe
+42	Fernanda	Lowe
+43	Fernanda	Lowe
+44	Fernanda	Fernanda
+45	Fernanda	acsfqwfcds
 \.
 
 
@@ -198,7 +238,7 @@ COPY users (id, name, password) FROM stdin;
 -- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Guest
 --
 
-SELECT pg_catalog.setval('users_id_seq', 29, true);
+SELECT pg_catalog.setval('users_id_seq', 45, true);
 
 
 --
